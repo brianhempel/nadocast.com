bebddab535a896751f198b52551c5d6a1a2806ca
Josiah Aviles <josiaha@uchicago.edu>
2021-06-23 18:13:25 -0500

    made more progress on the fb -> js translation

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 build:
 	ruby make_site.rb
-	node make_site.js
--- make_site.js
+++ make_site.js
@@ -1,7 +1,8 @@
 //import { writeFile } from 'fs';
 //writeFile('indox.html',"<h1>hello world</h1>", callbackFunction)
-var fs = require('fs');
-var files = fs.readdirSync('forecasts');
+const fs = require('fs');
+const path = require('path')
+const files = fs.readdirSync('forecasts');
 
 const formattedDates = [];
 
@@ -16,8 +17,43 @@ function ayyy(item,index) {
     let month = Number(month_str);
     let day = Number(day_str);
 
-    nice_date_str = `${year}-${month}-${day}`;
+    let nice_date_str = `${year}-${month}-${day}`;
     formattedDates.push(nice_date_str);
+
+    let forecast_date_dir   =  path.join('/','forecasts', item); // "forecasts/20210617"
+    let out_dir             =  path.join('/','site', nice_date_str); // "site/2021-6-17"
+
+    //Make directories if they don't exist yet
+    fs.mkdir(path.join(__dirname,out_dir), 
+        { recursive: true }, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('Directory created successfully!');
+    });
+
+    let run_hour_strs = []
+
+    fs.readdir(path.join(__dirname,forecast_date_dir), (err, folders) => {
+        folders.forEach(folder => {
+          run_hour_strs.push(folder);
+        });
+        run_hour_strs = run_hour_strs.sort(function(a,b) {
+            let aVal    = Number(a.match(/\d+/)[0]);
+            let bVal    = Number(b.match(/\d+/)[0]);
+            if (aVal < bVal) { return -1;}
+            if (aVal > bVal) { return 1;}
+            else { return 0 }
+        });
+        console.log("!!!!!!" + run_hour_strs);
+      });
+
+    let out = "";
+    out += `<html>\n`;
+    out += `<head><title>Nadocast ${nice_date_str}</title></head>\n`;
+    out += `<body>\n`;
+    out += `<h1>Nadocast ${nice_date_str}</h1>\n`;
+
 
 }
 files.forEach(ayyy);
